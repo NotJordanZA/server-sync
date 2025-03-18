a8a13a68cfb2fe6b9c630068ad473f693b4de849
NotJordanZA <63955741+NotJordanZA@users.noreply.github.com>
2025-03-18 11:24:46 +0200

    Delete frontend/schedules directory

--- frontend/schedules/daily_11-00.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-::UpdateTest1
-powershell.exe -ExecutionPolicy Bypass -File "C:\Users\jorda\Documents\GitHub\server-sync\frontend\scripts\multithreadedsync.ps1" -sessionUrl "ftp://test:pass@update" -remotePath "reDir" -localPath "locDir" -logPath "logDir" -fileMask ">5Y|*.tmp" -connections 2
